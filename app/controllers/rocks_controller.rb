f29fd066f9e7bd2a42c47f040f0a3b6bb77b7873
jin camou <jincamou@gmail.com>
2017-07-25 12:15:22 -0700

    add rocks controller

---
+++ app/controllers/rocks_controller.rb
@@ -0,0 +1,40 @@
+class RocksController < ApplicationController
+  def index
+    @rocks = Rock.all
+    json_response(@rocks)
+  end
+
+  def show
+    @rock = Rock.find(params[:id])
+    json_response(@rock)
+  end
+
+  def create
+    @rock = Rock.create!(rock_params)
+    json_response(@rock, :created)
+  end
+
+  def update
+    @rock = Rock.find(params[:id])
+    if @rock.update!(rock_params)
+      render status: 200, json: {
+        message: "Rock updated!"
+      }
+    end
+  end
+
+  def destroy
+    @rock = Rock.find(params[:id])
+    if @rock.destroy!
+      render status: 200, json: {
+        message: "Rock deleted!"
+      }
+    end
+  end
+
+  private
+  def rock_params
+    params.permit(:name)
+  end
+
+end
